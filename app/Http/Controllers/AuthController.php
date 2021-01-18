aefe31d17ed0bae16bb553b3873e804786f27586
ibalpalaloi <ibalpalaloi@gmil.com>
2021-01-18 15:42:09 +0800

    buat sign up

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule; 
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -25,7 +27,15 @@ class AuthController extends Controller
             'konfir_password' => Rule::in([$request->password])
         ]);
 
-        $enkripsi = crypt($request->email, $request->password);
-        dd($enkripsi);
+        // $enkripsi = crypt($request->email, $request->password);
+
+        $user = new User;
+        $user->username = $request->nama;
+        $user->password = bcrypt($request->password);
+        $user->email = $request->email;
+        $user->remember_token = str::random(60);
+        $user->level_akses = 'toko';
+        $user->save();
+        
     }
 }
