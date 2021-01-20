3e02a177a798eda9eebcfc4035ce32268faf3f86
ibalpalaloi <ibalpalaloi@gmil.com>
2021-01-20 11:50:55 +0800

    modif layout toko

--- resources/views/layouts/toko.blade.php
+++ resources/views/layouts/toko.blade.php
@@ -134,53 +134,6 @@
         <!-- ============================================================== -->
         <!-- Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
-<<<<<<< HEAD
-        <div class="navbar-default sidebar" role="navigation">
-            <div class="sidebar-nav slimscrollsidebar">
-                <div class="sidebar-head">
-                    <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span
-                            class="hide-menu">Navigation</span></h3>
-                </div>
-                <ul class="nav" id="side-menu">
-                    <li class="user-pro">
-                        <a href="#" class="waves-effect"><img
-                                src="{{asset('public/template/admin/plugins/images/users/varun.jpg')}}" alt="user-img"
-                                class="img-circle"> <span class="hide-menu">Toko <span class="fa arrow"></span></span>
-                        </a>
-                        <ul class="nav nav-second-level collapse" aria-expanded="false" style="height: 0px;">
-                            <li><a href="javascript:void(0)"><i class="ti-user"></i> <span class="hide-menu">My
-                                        Profile</span></a></li>
-                            <li><a href="javascript:void(0)"><i class="ti-wallet"></i> <span class="hide-menu">My
-                                        Balance</span></a></li>
-                            <li><a href="javascript:void(0)"><i class="ti-email"></i> <span
-                                        class="hide-menu">Inbox</span></a></li>
-                            <li><a href="javascript:void(0)"><i class="ti-settings"></i> <span class="hide-menu">Account
-                                        Setting</span></a></li>
-                            <li><a href="javascript:void(0)"><i class="fa fa-power-off"></i> <span
-                                        class="hide-menu">Logout</span></a></li>
-                        </ul>
-                    </li>
-                    <li> <a href="index.html" class="waves-effect"><i class="mdi mdi-av-timer fa-fw" data-icon="v"></i>
-                            <span class="hide-menu"> Toko <span class="fa arrow"></span> <span
-                                    class="label label-rouded label-inverse pull-right">4</span></span></a>
-                        <ul class="nav nav-second-level">
-                            <li> <a href="<?=url('toko/informasi_dasar')?>"><span class="hide-menu">Informasi Dasar</span></a> </li>
-                            <li> <a href="index2.html"><span class="hide-menu">Alamat Toko</span></a> </li>
-                            <li> <a href="index3.html"><span class="hide-menu">Akun</span></a> </li>
-                        </ul>
-                    </li>
-                    <li> <a href="index.html" class="waves-effect"><i class="mdi mdi-av-timer fa-fw" data-icon="v"></i>
-                        <span class="hide-menu"> Produk <span class="fa arrow"></span> <span
-                                class="label label-rouded label-inverse pull-right">4</span></span></a>
-                        <ul class="nav nav-second-level">
-                            <li> <a href="index.html"><span class="hide-menu">Semua Produk</span></a> </li>
-                            <li> <a href="index2.html"><span class="hide-menu">Tambah Produk</span></a> </li>
-                        </ul>
-                    </li>
-                    <li class="devider"></li>
-
-                </ul>
-=======
         <aside class="left-sidebar">
             <!-- Sidebar scroll-->
             <div class="scroll-sidebar">
