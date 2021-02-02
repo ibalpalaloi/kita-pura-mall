ab1c51ce0970774cb35da3b8e73a5c1a8626db44
andipa <andipabp@gmail.com>
2021-02-03 00:41:37 +0800

    update auth dan perbarui biodata

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
 }
