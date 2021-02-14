6d53a5ac2cbbdff383ec4be4abe1faa4c6d92068
ibalpalaloi <ibalpalaloi@gmil.com>
2021-02-14 22:18:17 +0800

    buat manajemen toko

--- app/Http/Controllers/Admin/Admin_Manajemen_Toko_Controller.php
+++ app/Http/Controllers/Admin/Admin_Manajemen_Toko_Controller.php
@@ -5,9 +5,24 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Kategori_toko;
+use App\Models\Daftar_tunggu_toko;
+use App\Models\Toko;
 
 class Admin_Manajemen_Toko_Controller extends Controller
 {
     //
-    
+    public function index(){
+        $toko = Toko::all();
+        return view('users.admin.m-toko.index', ['toko'=> $toko]);
+    }
+
+    public function daftar_tunggu_toko(){
+        $daftar_tunggu = Daftar_tunggu_toko::all();
+        return view('users.admin.m-toko.daftar_tunggu', ['daftar_toko'=>$daftar_tunggu]);
+    }
+
+    public function daftar_tunggu_toko_detail($id){
+        $toko = Daftar_tunggu_toko::find($id);
+        return view('users.admin.m-toko.daftar_tunggu_detail', ['toko'=>$toko]);
+    }
 }
---
+++ resources/views/users/admin/m-toko/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+
+
+@section('title')
+Toko
+@endsection
+
+@section('header-scripts')
+
+
+@endsection
+
+@section('modal')
+@endsection
+
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <div class="material-card card">
+            <div class="card-body">
+                <h4 class="card-title">Toko</h4>
+                <a type="button" class="btn btn-primary" href="<?=url('/')?>/admin/manajemen/daftar_tunggu_toko">
+                    Daftar Tunggu
+                </a>
+                <br><br>
+                <div class="table-responsive">
+                    <table id="zero_config" class="table table-striped border">
+                        <thead>
+                            <tr>
+                                <th>Nama Toko</th>
+                                <th>No Telp</th>
+                                <th>Alamat</th>
+                                <th>Kategori Toko</th>
+                                <th>Nama Pemilik</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($toko as $data)
+                                <tr>
+                                    <td>{{$data->nama_toko}}</td>
+                                    <td>{{$data->no_hp}}</td>
+                                    <td>{{$data->alamat}}</td>
+                                    <td>{{$data->kategori_toko->kategori}}</td>
+                                    <td>{{$data->nama_pemilik}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('footer-scripts')
+@endsection
