33358277e67d14e62728e013d45f64a05d5f597b
ibalpalaloi <ibalpalaloi@gmil.com>
2021-02-14 22:18:17 +0800

    buat manajemen toko

--- app/Models/toko.php
+++ app/Models/toko.php
@@ -5,8 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class toko extends Model
+class Toko extends Model
 {
     use HasFactory;
     protected $table = 'toko';
+
+    public function kategori_toko(){
+        return $this->belongsTo(Kategori_toko::class);
+    }
 }
