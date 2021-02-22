e3aadd5d82457fa32c1a88034d41e6f27d6b3783
ibalpalaloi <ibalpalaloi@gmil.com>
2021-02-22 14:15:02 +0800

    validasi daftar tunggu toko

--- app/Models/Daftar_tunggu_toko.php
+++ app/Models/Daftar_tunggu_toko.php
@@ -9,4 +9,12 @@ class Daftar_tunggu_toko extends Model
 {
     use HasFactory;
     protected $table = 'daftar_tunggu_toko';
+
+    public function user(){
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function kategori_toko(){
+        return $this->belongsTo(Kategori_toko::class);
+    }
 }
