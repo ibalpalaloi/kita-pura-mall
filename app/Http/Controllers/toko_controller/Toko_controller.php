42f2787e109ff5baa4b6ec8aa707909a68ece0b5
ibalpalaloi <ibalpalaloi@gmil.com>
2021-03-09 11:32:40 +0800

    perbaiki home dan toko

--- app/Http/Controllers/toko_controller/Toko_controller.php
+++ app/Http/Controllers/toko_controller/Toko_controller.php
@@ -11,26 +11,7 @@ class Toko_controller extends Controller
 {
     //
     public function index(Request $request){
-        // $list_toko = array();
-        // $toko = Toko::all();
-        // $i = 0;
-        // foreach($toko as $toko){
-        //     $foto_maps = Foto_maps::where('toko_id', $toko->id)->first();
-        //     if(!empty($foto_maps)){
-        //         $list_toko[$i]['foto'] = $foto_maps->foto;
-        //     }
-        //     else{
-        //         $list_toko[$i]['foto'] = '';
-        //     }
-        //     $list_toko[$i]['id_toko'] = $toko->id;
-        //     $list_toko[$i]['username'] = $toko->username;
-        //     $list_toko[$i]['nama_toko'] = $toko->nama_toko;
-        //     $list_toko[$i]['kategori_toko'] = $toko->kategori_toko->kategori;
-        //     $list_toko[$i]['jenis_mitra'] = $toko->jenis_mitra;
-        //     $list_toko[$i]['logo'] = $toko->logo_toko;
-        //     $i++;
-        // }
-        // return view('toko.dashboard', ['toko'=>$list_toko]);
+        
         $cari = $request->get('cari');
         if ($cari == 'all'){
             $tokos = Toko::paginate(5);
@@ -42,7 +23,7 @@ class Toko_controller extends Controller
         for($i = 0; $i < count($tokos); $i++){
             $foto = Foto_maps::where([
                                         ['toko_id', $tokos[$i]->id],
-                                        ['no_foto', 1]
+                                        ['no_foto', 3]
                                     ])->first();
             if(!empty($foto)){
                 $tokos[$i]->setAttribute('foto', $foto->foto);
