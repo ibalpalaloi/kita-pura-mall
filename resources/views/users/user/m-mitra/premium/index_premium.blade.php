6fd97ea5fc003e83356f81afe75eb440099f9882
andipa <andipabp@gmail.com>
2021-03-17 20:53:52 +0800

    Update Copy Link

--- resources/views/users/user/m-mitra/premium/index_premium.blade.php
+++ resources/views/users/user/m-mitra/premium/index_premium.blade.php
@@ -8,6 +8,14 @@
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
 integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 -->
+
+<meta property="og:url"           content="https://www.m.kitapura.com/{{$toko->username}}" />
+<meta property="og:type"          content="website" />
+<meta property="og:title"         content="KitaPura Mall | {{$toko->username}}" />
+<meta property="og:description"   content="KitaPura Mall" />
+<meta property="og:image"         content="{{asset('public/img/icon.png')}" />
+
+
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 <style type="text/css">
 	.banner {
@@ -273,6 +281,7 @@ integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6J
 	}
 
 </style>
+
 @endsection
 
 @section('content')
@@ -319,20 +328,25 @@ integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6J
 				<img src="<?=url('/')?>/public/img/mitra/modal_product.svg" style="width: 100%; position: absolute; top: -16em;">
 				<div style="font-size: 1.5em; font-weight: 600; margin-top: 3.5em;">Bagikan Website Kamu!</div>
 				<div style="font-size: 1em; text-align: center; width: 100%; font-weight: 0; color: #ffe6f1; margin-bottom: 0.5em;">
+
+					<div style="background: #1E1E1F; padding: 1em 1.5em; border-radius: 2em; position: relative; margin-top: 0.5em; margin-bottom: 0.5em;">
+						<input type="text" id="link_toko_2" style="cursor: pointer; color: white;width: 100%; background: #1E1E1F; font-weight: 500;" value="kitapura.com/{{$toko->username}}" readonly>
+					</div>
 					<div class="share_div" style="display: flex; justify-content: space-between; margin-top: 0.5em;">
-						<div class="share_platform">
+						<div id="salin-link-2" class="share_platform" data-toggle="tooltip" data-placement="bottom" title="Salin Link" style="cursor: pointer;" onclick="copy_text('2')">
 							<img src="<?=url('/')?>/public/img/icon_svg/copy_white.svg">
+							<input type="hidden" id="link_toko_2" value="kitapura.com/{{$toko->username}}" readonly>
 						</div>
-						<div class="share_platform" style="background: #3b5998;">
-							<i class="fab fa-facebook-f" style="font-size: 1.5em;"></i>
+						<div onclick="link_fb()" class="share_platform" style="background: #3b5998;">
+							<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.m.kitapura.com%2F{{$toko->username}}"><i class="fab fa-facebook-f text-white" style="font-size: 1.5em;"></i></a>
 						</div>
-						<div class="share_platform" style="background: #25d366; padding-bottom: 0.1em; padding-left: 0.1em;">
+						<div class="share_platform" onclick="WhatsappMessage()" style="background: #25d366; padding-bottom: 0.1em; padding-left: 0.1em;">
 							<i class="fab fa-whatsapp" style="font-size: 2em;"></i>
 						</div>
 
-						<div class="share_platform" style="background: #1da1f2; padding-left: 0.1em;">
+						<!-- <div class="share_platform" style="background: #1da1f2; padding-left: 0.1em;">
 							<i class="fab fa-twitter" style="font-size: 1.8em;"></i>
-						</div>
+						</div> -->
 					</div>
 				</div>
 			</div>
@@ -424,11 +438,14 @@ integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6J
 			</div>			
 		</div>
 		<div style="width: 100%; display: flex; justify-content: center;">
-			<div style="width: 80%; background: #1E1E1F; padding: 1em 1.5em; border-radius: 2em; position: relative; margin-top: 0.5em; margin-bottom: 0.5em;">
-				<input type="text" id="link_toko" style="color: white;width: 100%; background: #1E1E1F; font-weight: 500;" value="kitapura.com/{{$toko->username}}" readonly>
-				<div style="width: 2.5em; height: 2.5em; background: #DD9D25; border-radius: 50%; display: flex; justify-content: center; position: absolute; right: 0.6em; bottom: 0.6em;" onclick="share_button()"><img src="<?=url('/')?>/public/img/icon_svg/share.svg" style="width: 60%;"></div>
+			<div style="width: 80%; background: #1E1E1F; padding: 1em 1.5em; border-radius: 2em; position: relative; margin-top: 0.5em; margin-bottom: 0.5em;" >
+				<div id="salin-link-1" data-toggle="tooltip" data-placement="bottom" title="Salin Link" style="cursor: pointer;" onclick="copy_text('1')">
+					<input type="text" id="link_toko_1" style="cursor: pointer; color: white;width: 100%; background: #1E1E1F; font-weight: 500;" value="kitapura.com/{{$toko->username}}" readonly>
+				</div>
+				<div style="cursor: pointer; width: 2.5em; height: 2.5em; background: #DD9D25; border-radius: 50%; display: flex; justify-content: center; position: absolute; right: 0.6em; bottom: 0.6em;" onclick="share_button()"><img src="<?=url('/')?>/public/img/icon_svg/share.svg" style="width: 60%;"></div>
 			</div>
 		</div>
+
 		<div style="display: flex;justify-content:center;flex-direction: column; align-items: center; padding-bottom: 1.2em;">
 			<a href="<?=url('/')?>/akun/mitra/premium/atur-toko" style="padding-left: 0.4em;">
 				<img src="<?=url('/')?>/public/img/button/toko_premium/atur_toko.svg" style="width: 100%;">
@@ -473,6 +490,7 @@ integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6J
 @endsection
 
 @section('footer-scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
 integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
@@ -660,4 +678,72 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
 	}
 
 </script>
+
+<script>
+	function copy_text(id) {
+		var text = "Halo, Silahkan kunjungi toko saya di link berikut: "
+    	var url = document.getElementById("link_toko_"+id).value;
+		var copyurl = document.getElementById("link_toko_"+id);
+		copyurl.value = text + "https://www.m." + url;   
+		// alert(copyurl.value);
+		copyurl.select();
+		copyurl.setSelectionRange(0, 99999); /* For mobile devices */
+		document.execCommand("copy");
+		document.getElementById("link_toko_"+id).value = url;
+
+		$(document).ready(function(){
+            $('#salin-link-'+id).attr('title', 'Link Berhasil Disalin !')
+                  .tooltip('dispose')
+                  .tooltip({ title: 'Link Berhasil Disalin'});
+            $('#salin-link-'+id).tooltip('show');
+		});
+
+	}
+
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip();   
+	});
+
+	var isMobile = mobilecheck();
+
+	function mobilecheck() {
+		var check = false;
+		(function (a) {
+			if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i
+				.test(a) ||
+				/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+				.test(a.substr(0, 4))) check = true;
+		})(navigator.userAgent || navigator.vendor || window.opera);
+		return check;
+	}
+
+	function WhatsappMessage() {
+
+		var text = "Halo, Silahkan kunjungi toko saya di link berikut: "
+    	var url = document.getElementById("link_toko_2").value;
+		var copyurl = document.getElementById("link_toko_2");
+		var url = text + "https://www.m." + url;   
+		var apilink = 'http://';
+		var message = url;
+		apilink += isMobile ? 'api' : 'web';
+		apilink += '.whatsapp.com/send?text=' + encodeURI(message);
+
+		window.open(apilink);
+	}
+
+	function link_fb() {
+		var text = "Halo, Silahkan kunjungi toko saya di link berikut: "
+    	var url = document.getElementById("link_toko_2").value;
+		var copyurl = document.getElementById("link_toko_2");
+		copyurl.value = text + "https://www.m." + url;   
+		// alert(copyurl.value);
+		copyurl.select();
+		copyurl.setSelectionRange(0, 99999); /* For mobile devices */
+		document.execCommand("copy");
+		document.getElementById("link_toko_2").value = url;
+	}
+
+
+</script>
+
 @endsection
