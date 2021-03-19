d8ca4e64332531d666ad1af87dcfbb4cadf988da
thulbahri <mohfathulbahri@gmail.com>
2021-03-19 14:41:07 +0800

    tes

--- resources/views/landing_page/index.blade.php
+++ resources/views/landing_page/index.blade.php
@@ -364,11 +364,7 @@ style="padding: 1.5em; padding: 0px;">
 		@if(Auth()->user()->id == $toko->users_id)
 		<a onclick="show_loader()" id="defaultheader_logo" title="Kitabisa" style="margin-left: 20px; height:33px;margin-right:15px; position: relative;" href="<?=url('/')?>/akun/mitra/premium">
 			<img src="<?=url('/')?>/public/img/icon_svg/setting_white_bg.svg">
-		</a>
-		<div id="defaultheader_logo" title="Kitabisa" style="margin-left: 20px; height:33px;margin-right:20px; position: relative;" href="<?=url('/')?>/user/keranjang" onclick="cooming_soon()">
-			<img src="<?=url('/')?>/public/img/icon_svg/bag_circle_transparent.svg">
-			<div style="width: 1.5em; height: 1.5em; background:#9d0208; position: absolute;border-radius: 50%; bottom: -20px; right: 0; background: #FF0000; color: white; text-align: center;" id="jumlah_keranjang">{{count($keranjang)}}</div>
-		</div>		
+		</a>		
 		@else
 		<a id="defaultheader_logo" title="Kitabisa" style="margin-left: 20px; height:33px;margin-right:20px; position: relative;" href="<?=url('/')?>/user/keranjang">
 			<img src="<?=url('/')?>/public/img/icon_svg/bag_circle_transparent.svg">
