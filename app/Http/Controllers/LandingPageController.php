ce2af4b21fcbe9743c67daadc3c7aa12fb636f95
ibalpalaloi <ibalpalaloi@gmil.com>
2021-03-20 21:39:41 +0800

    color status bar

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -16,7 +16,8 @@ use App\Models\Template_landing_page;
 
 class LandingPageController extends Controller
 {
-	public function landing_page_mitra($mitra){
+	public function landing_page_mitra(Request $request, $mitra){
+		
 		$keranjang = Keranjang_belanja::where('user_id', Auth()->user()->id)->get();
 		$toko = Toko::where('username', $mitra)->first();
 		$produk = Product::where('toko_id', $toko->id)->where('tampil','ya')->get();
@@ -74,6 +75,11 @@ class LandingPageController extends Controller
 			$page->save();
 		}
 		$landing_page = Landing_page_toko::where('toko_id', $toko->id)->first();
+		$color_status_bar = $request->get('colorStatusBar');
+		$color_code = $str = ltrim($landing_page->warna_header, '#'); 
+		if(is_null($color_status_bar)){
+			return redirect(url()->current()."?colorStatusBar=".$color_code);
+		}
 		return view('landing_page/index', compact('toko', 'produk', 'keranjang', 'penilaian', 'rating', 'foto_map', 'fasilitas', 'video', 'landing_page'));
 	}
 
