ded02d2fb596f5649836d3ad37785f3b45b002de
ibalpalaloi <ibalpalaloi@gmil.com>
2021-03-22 12:36:59 +0800

    perbaiki percarian

--- resources/views/layouts/home_no_menu.blade.php
+++ resources/views/layouts/home_no_menu.blade.php
@@ -43,6 +43,7 @@
 	@yield("content")
 </body>    
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script>
 	function show_loader(){
 		console.log('show');
