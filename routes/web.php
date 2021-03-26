25f2eef0eccb017d428da733d7bda71205e2c6d1
ibalpalaloi <ibalpalaloi@gmil.com>
2021-03-26 11:11:58 +0800

    admin manajemen toko

--- routes/web.php
+++ routes/web.php
@@ -232,6 +232,7 @@ Route::group(['middleware'=> 'auth'], function() {
         Route::get('/admin/manajemen/toko', [Admin_Manajemen_Toko_Controller::class, 'index']);
         Route::get('/admin/manajemen/toko/{id}', [Admin_Manajemen_Toko_Controller::class, 'detail_toko']);
         Route::post('/admin/manajemen/toko/{id}/post_ubah', [Admin_Manajemen_Toko_Controller::class, 'post_ubah_toko']);
+        Route::post('/admin/manajemen/toko/{id}/post_ubah_alamat', [Admin_Manajemen_Toko_Controller::class, 'post_ubah_alamat_toko']);
         Route::get('/admin/manajemen/daftar_tunggu_toko', [Admin_Manajemen_Toko_Controller::class, 'daftar_tunggu_toko']);
         Route::get('/admin/manajemen/daftar_tunggu_toko/{id}', [Admin_Manajemen_Toko_Controller::class, 'daftar_tunggu_toko_detail']);
         Route::post('/admin/manajemen/daftar_tunggu_toko/post', [Admin_Manajemen_Toko_Controller::class, 'post_daftar_tunggu_toko']);
